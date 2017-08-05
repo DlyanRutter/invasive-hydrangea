311c58937e9dee504d4ed9c0f3fe31fdd9865470
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-08-05 14:55:29 -0500

    Update invasive_species.py
    Added descriptions to functions

--- invasive_species.py
+++ invasive_species.py
@@ -2,7 +2,6 @@ import os, keras
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-#from keris.utils.visualize_util import plot
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from keras.preprocessing.image import img_to_array, load_img, \
      ImageDataGenerator, NumpyArrayIterator, array_to_img
@@ -17,6 +16,10 @@ root = '../input'
 np.random.seed(1337)
 
 def train_data():
+    """
+    Loads training data from data file into a Pandas dataframe. Returns labels (whether or not a hydrangea is presetn)
+    as well as ID numbers.
+    """
     data = pd.read_csv(os.path.join(root, '/Users/dylanrutter/Downloads/train_labels.csv'))
     sample = data.pop('name')
     invasive = data.pop('invasive')
@@ -31,6 +34,11 @@ def resize_image(img, max_dim=96):
     return img.resize((int(bigger*scale), int(smaller*scale)))
 
 def image_train(ids, max_dim=96, center=True):
+    """
+    Loads images from train dataset. Converts each image into an array and places the arrays into a matrix. The dimensions
+    of the images are designated by "max_dim" argument, and whether or not the image is centered is designated by "center"
+    argument.
+    """
     X = np.empty((len(ids), max_dim, max_dim, 1))
     for i, idee in enumerate(ids):
             x = resize_image(load_img(os.path.join(root,'/Users/dylanrutter/Downloads/train', str(idee) + '.jpg'), grayscale=True), max_dim=max_dim)
@@ -50,6 +58,11 @@ def image_train(ids, max_dim=96, center=True):
     return np.around(X / 255.0)
 
 def image_test(ids, max_dim=96, center=True):
+    """
+    Loads images from test dataset. Converts each image into an array and places the arrays into a matrix. The dimensions
+    of the images are designated by "max_dim" argument, and whether or not the image is centered is designated by "center"
+    argument.
+    """
     X = np.empty((len(ids), max_dim, max_dim, 1))
     for i, idee in enumerate(ids):
             x = resize_image(load_img(os.path.join(root,'/Users/dylanrutter/Downloads/test', str(idee) + '.jpg'), grayscale=True), max_dim=max_dim)
