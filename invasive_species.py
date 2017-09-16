6711f0e5a52f3d13486f3a7bad1c0662bc03f23c
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-09-15 19:35:51 -0500

    Update invasive_species.py
    Updates and alternative methods for loading

--- invasive_species.py
+++ invasive_species.py
@@ -13,6 +13,68 @@ split = 0.9
 root = '../input'
 np.random.seed(1337)
 
+root = '../input'
+np.random.seed(1337)
+LR = 1e-3
+
+def load_train():
+    """
+    one_hot will return [1,0] if the species is not invasive, [0,1] if it is.
+    """
+    path = '../input/train/'
+    all_labels_set = pd.read_csv(os.path.join\
+                         (root,'/Users/dylanrutter/Downloads/train_labels.csv'))
+    train_labels = np.array(all_labels_set['invasive'].iloc[: ])
+    train_files = []
+    one_hot = []
+
+    for i in range(len(all_labels_set)):
+        train_files.append(path + str(int(all_labels_set.iloc[i][0])) + '.jpg')
+        
+        if all_labels_set.iloc[i][1] == 0:
+            one_hot.append([1,0])
+        else:
+            one_hot.append([0,1])
+            
+    all_labels_set['name'] = train_files
+    all_labels_set['one_hot'] = one_hot
+    return train_files, train_labels, all_labels_set
+    
+train_files, train_labels, all_labels_set = load_train()
+
+def get_train_arrays():
+    """
+    converts df segments into numpy arrays
+    """
+    files, labels, df = load_train()
+    name = np.array(df.pop('name'))
+    invasive = np.array(df.pop('invasive'))
+    one_hot_label = np.array(df.pop('one_hot'))
+    return name, invasive, one_hot_label
+
+def label_img(img):
+    label = img.split('.')[-2]
+    if label == 0: return [1, 0]
+    if label == 1: return [0, 1]
+    
+train_name, train_invasive, train_one_hot = get_train_arrays()
+
+MODEL_NAME = 'invasiveplants -{}-{}.model'.format(LR, '2conv')
+
+def load_imgs(IMG_SIZE=96):
+    train_data = []
+    train_dir = '/Users/dylanrutter/Downloads/train'
+    for img in os.listdir(train_dir):
+        label = label_img(img)
+        path = os.path.join(train_dir, img)
+        img = cv2.resize(cv2.imread(path, cv2.IMREAD_COLOR),(IMG_SIZE,IMG_SIZE))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        train_data.append([np.array(img), np.array(label)])
+ #   shuffle(train_data)
+    np.save('train_data.npy', train_data)
+    return train_data
+
+
 def train_data():
     """
     Loads training data from data file into a Pandas dataframe. Returns labels (whether or not a hydrangea is presetn)
